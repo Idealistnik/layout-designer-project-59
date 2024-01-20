767a299ef7ccb441fada7dd4d9c6ce3135fbec80
Pavlov Nikita <nikitapavlov2015@gmail.com>
2024-01-20 03:18:16 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const {
 } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
+const svgSprite = require('gulp-svg-sprite');
 const browserSync = require('browser-sync').create();
 
 // const browserSyncJob = () => {
@@ -45,10 +46,36 @@ const watchers = () => {
 const copyFile = () => {
   return src('app/images/**/*')
     .pipe(dest('build/images'));
+}
+
+// const svgspriteConfig = {
+//   mode: {
+//     css: { // Activate the «css» mode
+//       render: {
+//         css: true // Activate CSS output (with default options)
+//       }
+//     }
+//   }
+// };
+
+const svgspriteConfig = {
+  mode: {
+    stack: {
+        sprite: "../sprite.svg"  //sprite file name
+    }
+  },
+}
+
+const buildSvg = () => {
+  return src('app/images/icons/**/*.svg')
+    .pipe(svgSprite(svgspriteConfig))
+    .pipe(dest('build/images/icons'));
 };
+
 
 exports.copy = copyFile;
 exports.watch = watchers;
 exports.buildPug = buildPug;
+exports.buildSvg = buildSvg;
 // exports.server = browserSyncJob;
-exports.build = parallel(buildSass, buildPug, copyFile);
+exports.build = parallel(buildSass, buildPug, copyFile, buildSvg);
